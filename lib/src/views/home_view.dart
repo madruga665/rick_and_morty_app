83fed62bc39afda881d52edd77cbb2d8345de09a
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-10-08 22:35:26 -0300

    feat(HomeView): add HomeView

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty_app/src/models/character_model.dart';
+import 'package:rick_and_morty_app/src/repository/character_repository.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key, required this.title});
@@ -10,6 +12,19 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  List<CharacterModel> characters = [];
+  CharacterRepository characterRepository = CharacterRepository();
+
+  void getAllCharacters() async {
+    characters = await characterRepository.getAll();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getAllCharacters();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +32,14 @@ class _HomeViewState extends State<HomeView> {
           title: Text(widget.title),
         ),
         body: Center(
-          child: Text('${widget.title} app'),
+          child: ListView.builder(
+              itemCount: characters.length,
+              itemBuilder: ((context, index) {
+                var character = characters[index];
+                return ListTile(
+                  title: Text(character.name),
+                );
+              })),
         ));
   }
 }
