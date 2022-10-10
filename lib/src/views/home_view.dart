888e81fc8d83637fa696b395584ffd50a21b77e6
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-10-10 00:12:53 -0300

    feat(HomeView): add list view builder para renderizar personagens

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:rick_and_morty_app/src/models/character_model.dart';
-import 'package:rick_and_morty_app/src/repository/character_repository.dart';
+import 'package:rick_and_morty_app/src/components/character_card.dart';
+import 'package:rick_and_morty_app/src/repositories/character_repository.dart';
+
+import '../models/character_model.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key, required this.title});
-
   final String title;
 
   @override
@@ -12,17 +13,13 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  List<CharacterModel> characters = [];
-  CharacterRepository characterRepository = CharacterRepository();
-
-  void getAllCharacters() async {
-    characters = await characterRepository.getAll();
-  }
+  late Future<List<CharacterModel>> characters;
+  final CharacterRepository characterRepository = CharacterRepository();
 
   @override
   void initState() {
     super.initState();
-    getAllCharacters();
+    characters = characterRepository.getAll();
   }
 
   @override
@@ -32,14 +29,32 @@ class _HomeViewState extends State<HomeView> {
           title: Text(widget.title),
         ),
         body: Center(
-          child: ListView.builder(
-              itemCount: characters.length,
-              itemBuilder: ((context, index) {
-                var character = characters[index];
-                return ListTile(
-                  title: Text(character.name),
-                );
-              })),
-        ));
+            child: FutureBuilder<List<CharacterModel>>(
+          future: characters,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+                  child: ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        child: CharacterCard(
+                            character: snapshot.data!.elementAt(index)),
+                      );
+                    },
+                  ));
+            } else if (snapshot.hasError) {
+              return ElevatedButton(
+                  onPressed: () => setState(() {
+                        characters = characterRepository.getAll();
+                      }),
+                  child: const Text('TENTAR NOVAMENTE'));
+            }
+            return const CircularProgressIndicator();
+          },
+        )));
   }
 }
