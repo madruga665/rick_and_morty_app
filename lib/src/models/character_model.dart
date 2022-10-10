b8184f7c9e286510c907d4346bfc26494954da4c
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-10-10 00:10:36 -0300

    chore(characterModel): geração dos metodos do model automaticos

--- lib/src/models/character_model.dart
+++ lib/src/models/character_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class CharacterModel {
   final int id;
   final String name;
@@ -5,14 +7,24 @@ class CharacterModel {
 
   CharacterModel(this.id, this.name, this.image);
 
-  CharacterModel.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        name = json['name'],
-        image = json['image'];
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'image': image,
+    };
+  }
 
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'name': name,
-        'image': image,
-      };
+  factory CharacterModel.fromMap(Map<String, dynamic> map) {
+    return CharacterModel(
+      map['id']?.toInt() ?? 0,
+      map['name'] ?? '',
+      map['image'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CharacterModel.fromJson(String source) =>
+      CharacterModel.fromMap(json.decode(source));
 }
