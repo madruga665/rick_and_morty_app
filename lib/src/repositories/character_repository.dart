fbdd5481d3fd77778bfe5bdf28743afcaaeefc8d
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-10-10 00:11:38 -0300

    feat(CharacterRepository): add repository

---
+++ lib/src/repositories/character_repository.dart
@@ -0,0 +1,21 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:rick_and_morty_app/src/models/character_model.dart';
+
+class CharacterRepository {
+  final String _url = 'https://rickandmortyapi.com/api/character/';
+  final _dio = Dio();
+
+  Future<List<CharacterModel>> getAll() async {
+    try {
+      final response = await _dio.get(_url);
+      final charactersList = response.data['results'] as List;
+
+      return charactersList.map((map) => CharacterModel.fromMap(map)).toList();
+    } on DioError catch (e) {
+      log('Error: ${e.error}');
+      throw Exception('Erro ao buscar personagem');
+    }
+  }
+}
